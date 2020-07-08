12f3764038ea9aaaf97128592b0effdcdaf5a09f
Rick <rick@rickmarker.dk>
2020-07-08 21:27:42 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const aws = require('aws-sdk');
+const stepfunctions = new aws.StepFunctions();
+
+exports = (main) => {
+  return async ({TaskToken: taskToken, ...event}) => {
+      let params = {
+          taskToken: taskToken
+      };
+      
+      try{
+          params.output = JSON.stringify(await main(event));
+          await stepfunctions.sendTaskSuccess(params).promise();
+      } catch(error) {
+          params.output = error.message;
+          await stepfunctions.sendTaskFailure(params).promise();
+      }
+  };
+};
